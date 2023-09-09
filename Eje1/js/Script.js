6c345ff1aeba9487da3c1e202b80789a9d3b4d37
rodriquelali <rodriquelali@gmail.com>
2023-09-09 18:52:14 -0400

    var, let, const. introduccion a javascript

---
+++ Eje1/js/Script.js
@@ -0,0 +1,34 @@
+//como crear las variables
+//a=2 --> esta Mal
+//var a=2; --> var dependiendo de la situiacion o del problema;
+//let a = 2; --> es mejor
+
+
+
+
+let btn= document.getElementById("btnOpe");
+btn.addEventListener("click", function () {
+    let a,b,c;
+    a = parseInt(document.getElementById("txt1").value);
+    b = parseInt(document.getElementById("txt2").value);
+    c=a+b;
+    document.getElementById("res").innerHTML = `<h1>La suma es: ${c}</h1>`;
+   
+    document.getElementById("txt1").style.display = "none";
+    document.getElementById("txt2").value = "";
+});
+
+
+
+// function suma() {
+//     let a,b,c;
+//     a = parseInt(document.getElementById("txt1").value);
+//     b = parseInt(document.getElementById("txt2").value);
+//     c=a+b;
+//     document.getElementById("res").innerHTML = `<h1>La suma es: ${c}</h1>`;
+   
+//     document.getElementById("txt1").style.display = "none";
+//     document.getElementById("txt2").value = "";
+//     //alert(c);   
+// }
+
---
+++ var,let,const/index.js
@@ -0,0 +1,50 @@
+
+//var, solo se utiliza pat variables globales, 
+
+// var x =1;
+// if(x==1){
+//     
+//     var x=2;
+//     console.log(x);
+// }
+//console.log(x);
+
+//let, variable local.
+// let x =2
+
+// if(x==2){
+    
+//     let x=3;
+//     console.log(x); //3
+// }
+
+// console.log(x); //2
+
+// let h =3;
+// h =2;
+// function suma(x) {
+   
+// }
+
+// //constante, const
+// const pi = 3.14
+// console.log(pi);
+
+//Hoisting
+
+// console.log(sum)
+// var a =2, b=3
+
+// var sum = a+b
+
+
+
+function suma() {
+    var a =2, b=3
+    return a+b;
+}
+
+console.log(suma());
+
+
+
