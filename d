e3eaf0ed018fb7c7601cbb0b827ab31935be45c4
rodriquelali <rodriquelali@gmail.com>
2023-10-31 10:05:48 -0400

    objetos, estudiantes promedio de edad

---
+++ d
@@ -0,0 +1,29 @@
+[33mcommit 70243f9f609928453dd79324d60459ab83d34186[m[33m ([m[1;36mHEAD -> [m[1;32mproyecto/rodrigo[m[33m, [m[1;31morigin/proyecto/rodrigo[m[33m)[m
+Author: rodriquelali <rodriquelali@gmail.com>
+Date:   Fri Oct 27 21:47:38 2023 -0400
+
+    objetos, parte 2
+
+[33mcommit d5aca84c127d76ed79f3e4b8db49e1dd949010ce[m
+Author: rodriquelali <rodriquelali@gmail.com>
+Date:   Fri Oct 27 21:15:30 2023 -0400
+
+    objetos, for, whie, if
+
+[33mcommit 7645934f336038bcbc81ee0e26bc1736a28181cd[m
+Author: rodriquelali <rodriquelali@gmail.com>
+Date:   Sat Sep 16 19:56:44 2023 -0400
+
+    for, if, while
+
+[33mcommit 6c345ff1aeba9487da3c1e202b80789a9d3b4d37[m[33m ([m[1;32mmain[m[33m)[m
+Author: rodriquelali <rodriquelali@gmail.com>
+Date:   Sat Sep 9 18:52:14 2023 -0400
+
+    var, let, const. introduccion a javascript
+
+[33mcommit b7de40fbc515494dc643b28dba3cfea2ac5df4d1[m
+Author: rodriquelali <rodriquelali@gmail.com>
+Date:   Sat Sep 9 18:50:41 2023 -0400
+
+    first commit
--- objetos/script.js
+++ objetos/script.js
@@ -30,16 +30,43 @@ let productoFrutas = [{
 }]
 
 
-for (let index = 0; index < productoFrutas.length; index++) {
+// for (let index = 0; index < productoFrutas.length; index++) {
    
-    console.log(productoFrutas[index].precio);
-    for (let i = 0; i < productoFrutas[index].precio.length; i++) {
-        const element = productoFrutas[index].precio[i];
-        console.log(element);
-    }
-}
+//     console.log(productoFrutas[index].precio);
+//     for (let i = 0; i < productoFrutas[index].precio.length; i++) {
+//         const element = productoFrutas[index].precio[i];
+//         console.log(element);
+//     }
+// }
 
 //let nombre = "hola rdorigo"
 //console.log(nombre.length);
 //console.log(productoFrutas);
 
+//crear un objeto de profesion.
+//iterar , dos registros.
+
+let estudiante =[
+    {nombre: "pepe",
+    edad: 23,
+    telefono: [234,4546,5657,444,5]},
+    {nombre: "juan",
+    edad: 45,
+    telefono: [234,4546,5657]}
+
+]
+let pro = 0;
+let aux = 0;
+for (let index = 0; index < estudiante.length; index++) {
+    const element = estudiante[index].nombre;
+    pro= pro +estudiante[index].edad;
+    aux += 1;
+    console.log(element);
+    for (let j = 0; j < estudiante[index].telefono.length; j++) {
+        const element2 = estudiante[index].telefono[j];
+        console.log(element2);
+        
+    }
+    
+}
+console.log("el promedio: ", pro/aux);
